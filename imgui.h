a3a99710899cbbcdebf8b944da1d41845b432f2d
varigeri92 <44337802+varigeri92@users.noreply.github.com>
2025-12-09 14:41:40 +0100

    imgui API update

--- imgui.h
+++ imgui.h
@@ -79,7 +79,6 @@ Index of this file:
 #include <stdarg.h>                 // va_list, va_start, va_end
 #include <stddef.h>                 // ptrdiff_t, NULL
 #include <string.h>                 // memset, memmove, memcpy, strlen, strchr, strcpy, strcmp
-#include "API.h"
 // Define attributes of all API symbols declarations (e.g. for DLL under Windows)
 // IMGUI_API is used for core imgui functions, IMGUI_IMPL_API is used for the default backends files (imgui_impl_xxx.h)
 // Using dear imgui via a shared library is not recommended: we don't guarantee backward nor forward ABI compatibility + this is a call-heavy library and function call overhead adds up.
